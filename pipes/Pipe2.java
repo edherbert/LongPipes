976c50e035e86affecd65a578aa3ea3acbc92ab3
edherbert <edwardherbert802@yahoo.com>
2017-11-29 01:06:24 +0000

    Removed some duplicate comments

--- pipes/Pipe2.java
+++ pipes/Pipe2.java
@@ -13,15 +13,12 @@ public class Pipe2 extends Pipe{
     public boolean fitsRequirements(RequirementsInfo req){
         boolean fits = true;
         
-        //Check all the aspects of the pipe to find out if it fits the requirements.
         if(req.getPlasticGrade() < 2 || req.getPlasticGrade() > 4) fits = false;
         
         if(req.getColourPrint() == 0 || req.getColourPrint() == 2) fits = false;
         
         if(req.getInnerInsulation()) fits = false;
         if(req.getOuterReinforcement()) fits = false;
-        
-        //Don't bother checking chemical resistance as it can be set either way and the pipe would still be possible.
         
         return fits;
     }
--- pipes/Pipe5.java
+++ pipes/Pipe5.java
@@ -13,15 +13,12 @@ public class Pipe5 extends Pipe{
     public boolean fitsRequirements(RequirementsInfo req){
         boolean fits = true;
         
-        //Check all the aspects of the pipe to find out if it fits the requirements.
         if(req.getPlasticGrade() < 3 || req.getPlasticGrade() > 5) fits = false;
         
         if(req.getColourPrint() == 0 || req.getColourPrint() == 1) fits = false;
         
         if(req.getInnerInsulation()) fits = true;
         if(req.getOuterReinforcement()) fits = true;
-        
-        //Don't bother checking chemical resistance as it can be set either way and the pipe would still be possible.
         
         return fits;
     }
