94fd240d645aa1ef42e09a05ba57baf9eab4e3bc
johnrhyswilliams97 <UP800456@uni.ds.port.ac.uk>
2017-12-05 16:43:44 +0000

    Final change?
    I ran the clean + build, and it suggested another change.

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import System.Session;
 import pipes.RequirementsInfo;
 
 class Main{
-    public static void main(String args[]){
+    public static void main(String args[]) throws Exception{
         //Create an ordering session.
         //The session is also in-charge of it's ui.
         Session s = new Session();
